f3a385951a67f9bd26ec78ffe5d20bc2d99cf4eb
tumanob <tumanob@gmail.com>
2015-04-26 12:03:57 +0600

    adding faicons
    man icons for tablet and web

--- wp-content/themes/libkg/header.php
+++ wp-content/themes/libkg/header.php
@@ -15,6 +15,24 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<link rel="apple-touch-icon" sizes="57x57" href="/ficon/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/ficon/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/ficon/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/ficon/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/ficon/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/ficon/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/ficon/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/ficon/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/ficon/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="/ficon/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/ficon/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="/ficon/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/ficon/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/ficon/manifest.json">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="/ficon/mstile-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
 <?php wp_head(); ?>
 </head>
 
