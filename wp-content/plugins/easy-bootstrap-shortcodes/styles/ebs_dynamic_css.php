85a9894f81657d1d0955040ddbad9aa6159cfd2f
tumanob <tumanob@gmail.com>
2015-05-28 16:26:55 +0600

    updates plugins

--- wp-content/plugins/easy-bootstrap-shortcodes/styles/ebs_dynamic_css.php
+++ wp-content/plugins/easy-bootstrap-shortcodes/styles/ebs_dynamic_css.php
@@ -1,7 +1,6 @@
 <?php
 header("Content-type: text/css");
-if(!session_id())
-    ebs_session_start();
+if(!session_id()) @session_start();
 //echo $_SESSION['ebs_dynamic_css'];
 if(isset($_SESSION['ebs_dynamic_css'])){
     echo $_SESSION['ebs_dynamic_css'];
@@ -13,4 +12,4 @@ if(isset($_SESSION['ebs_slider_css'])){
     }
 }
 
-session_write_close();
+//session_write_close();
